9082786d98faf8fc81f1df8dc39edd756f2ef937
Miranda <lfelipee.mi@gmail.com>
2026-05-05 20:13:22 -0400

    Add shell_Sort include header

---
+++ shell_Sort/include/shell_Sort.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_SORT_H
+#define SHELL_SORT_H
+
+/**
+ * Ordena um vetor utilizando o algoritmo Shell Sort.
+ * 
+ * @param v Ponteiro para o vetor a ser ordenado.
+ * @param n Tamanho do vetor.
+ */
+void shell_sort(int *v, int n);
+
+#endif // SHELL_SORT_H
